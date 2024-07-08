6e6d62616f23df1264774fa59ae27eb9dab4c200
Atharv <atharvakumar0@gmail.com>
2024-07-09 00:28:48 +0530

    Initial commit with basic backend setup

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db.js');
+const authRoutes = require('./routes/auth.js');
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+const port = process.env.PORT || 3000; 
+app.listen(port, () => {
+    console.log(`server is connected to port ${port}`)
+})
